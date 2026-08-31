1f651b3e39ef18265b0e51c96d6cfb513d577bac
Pirate13Thebes <c.gnagne@alustudent.com>
2026-08-31 07:05:25 +0000

    Add api_advanced folder

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,24 +1,26 @@
 #!/usr/bin/python3
-"""Query the Reddit API for a subreddit's subscriber count."""
+"""Query the Reddit API to get the number of subscribers."""
+
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Return the number of subscribers for a given subreddit.
-
-    If the subreddit is invalid, return 0.
-    """
+    """Return the number of subscribers for a subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "alche-scripting:api-advanced:v1.0 (by /u/alche_student)"}
 
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    headers = {
+        "User-Agent": "Mozilla/5.0"
+    }
+
+    response = requests.get(
+        url,
+        headers=headers,
+        allow_redirects=False
+    )
 
     if response.status_code != 200:
         return 0
 
-    try:
-        data = response.json().get("data", {})
-    except ValueError:
-        return 0
+    data = response.json()
 
-    return data.get("subscribers", 0)
+    return data["data"]["subscribers"]
